49b25bf5f2742ce31ee21d3c88d7eadc41fca453
Zahaby18 <achmadzahaby2@gmail.com>
2026-09-12 16:37:58 +0700

    Revisi 02-functions.php

--- latihan/02-functions.php
+++ latihan/02-functions.php
@@ -30,8 +30,8 @@ echo "\n";
 echo number_format(1500000, 0, ",", ".") . "\n";
 
 function formatRupiah(float $angka): string{
-    return "Rp." . number_format($angka, 0, ",", "."). ",-";
+    return "Rp " . number_format($angka, 0, ",", "."). ",-";
 }
 
-echo formatRupiah(15000);
+echo formatRupiah(15000000). "\n";
 echo formatRupiah(85000);
